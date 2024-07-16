659dbbc3c503d6e3e009068f93ee9d21ab2fde10
Jayesh Bandi <154779425+JayCodeGeek@users.noreply.github.com>
2024-07-16 20:04:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,124 @@
+var indianStarter = document.getElementById("indianStarter");
+var indianMain = document.getElementById("indianMain");
+
+var italianStarter = document.getElementById("italianStarter");
+var italianMain = document.getElementById("italianMain");
+
+var thaiStarter = document.getElementById("thaiStarter");
+var thaiMain = document.getElementById("thaiMain");
+
+var frenchStarter = document.getElementById("frenchStarter");
+var frenchMain = document.getElementById("frenchMain");
+
+var greekStarter = document.getElementById("greekStarter");
+var greekMain = document.getElementById("greekMain");
+
+var japaneseStarter = document.getElementById("japaneseStarter");
+var japaneseMain = document.getElementById("japaneseMain");
+
+
+italianStarter.style.display = "none";
+italianMain.style.display = "none";
+
+thaiStarter.style.display = "none";
+thaiMain.style.display = "none";
+
+frenchStarter.style.display = "none";
+frenchMain.style.display = "none";
+
+greekStarter.style.display = "none";
+greekMain.style.display = "none";
+
+japaneseStarter.style.display = "none";
+japaneseMain.style.display = "none";
+
+
+function indianVisible(){
+    indianStarter.style.display = "block";
+    indianMain.style.display = "block";
+    italianStarter.style.display = "none";
+    italianMain.style.display = "none";
+    thaiStarter.style.display = "none";
+    thaiMain.style.display = "none";
+    frenchStarter.style.display = "none";
+    frenchMain.style.display = "none";
+    greekStarter.style.display = "none";
+    greekMain.style.display = "none";
+    japaneseStarter.style.display = "none";
+    japaneseMain.style.display = "none";
+}
+
+function italianVisible(){
+    indianStarter.style.display = "none";
+    indianMain.style.display = "none";
+    italianStarter.style.display = "block";
+    italianMain.style.display = "block";
+    thaiStarter.style.display = "none";
+    thaiMain.style.display = "none";
+    frenchStarter.style.display = "none";
+    frenchMain.style.display = "none";
+    greekStarter.style.display = "none";
+    greekMain.style.display = "none";
+    japaneseStarter.style.display = "none";
+    japaneseMain.style.display = "none";
+}
+
+function thaiVisible(){
+    indianStarter.style.display = "none";
+    indianMain.style.display = "none";
+    italianStarter.style.display = "none";
+    italianMain.style.display = "none";
+    thaiStarter.style.display = "block";
+    thaiMain.style.display = "block";
+    frenchStarter.style.display = "none";
+    frenchMain.style.display = "none";
+    greekStarter.style.display = "none";
+    greekMain.style.display = "none";
+    japaneseStarter.style.display = "none";
+    japaneseMain.style.display = "none";
+}
+
+function frenchVisible(){
+    indianStarter.style.display = "none";
+    indianMain.style.display = "none";
+    italianStarter.style.display = "none";
+    italianMain.style.display = "none";
+    thaiStarter.style.display = "none";
+    thaiMain.style.display = "none";
+    frenchStarter.style.display = "block";
+    frenchMain.style.display = "block";
+    greekStarter.style.display = "none";
+    greekMain.style.display = "none";
+    japaneseStarter.style.display = "none";
+    japaneseMain.style.display = "none";
+}
+
+function greekVisible(){
+    indianStarter.style.display = "none";
+    indianMain.style.display = "none";
+    italianStarter.style.display = "none";
+    italianMain.style.display = "none";
+    thaiStarter.style.display = "none";
+    thaiMain.style.display = "none";
+    frenchStarter.style.display = "none";
+    frenchMain.style.display = "none";
+    greekStarter.style.display = "block";
+    greekMain.style.display = "block";
+    japaneseStarter.style.display = "none";
+    japaneseMain.style.display = "none";
+}
+
+function japaneseVisible(){
+    indianStarter.style.display = "none";
+    indianMain.style.display = "none";
+    italianStarter.style.display = "none";
+    italianMain.style.display = "none";
+    thaiStarter.style.display = "none";
+    thaiMain.style.display = "none";
+    frenchStarter.style.display = "none";
+    frenchMain.style.display = "none";
+    greekStarter.style.display = "none";
+    greekMain.style.display = "none";
+    japaneseStarter.style.display = "block";
+    japaneseMain.style.display = "block";
+}
